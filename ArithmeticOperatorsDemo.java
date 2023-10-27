b645eb3ded6c608abd832e152f70d557af34a67a
kariyappareddyLikhitha <kariyappareddyLikhitha@gmail.com>
2023-10-27 13:13:21 +0530

    modified AruthmeticOperations file

--- ArithmeticOperatorsDemo.java
+++ ArithmeticOperatorsDemo.java
@@ -4,6 +4,7 @@ public static void main(String[] args)
 {
 int x=5,y=2;
 System.out.println(x+y);
+System.out.println(x+y);
 System.out.println(x-y);
 System.out.println(x*y);
 System.out.println(x/y);
